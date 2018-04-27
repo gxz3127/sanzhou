48d262e9f3eb1be7ca3a533c9e1295518445e063
guomaomi <982548809@qq.com>
2018-04-27 11:31:11 +0800

    配置gulp

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp=require("gulp");
+
+var server=require("gulp-webserver");
+
+var data=require("./src/data/data.json"); 
+
+gulp.task("default",function(){
+    gulp.src("src")
+    .pipe(server({
+        port:"8070",
+        host:"169.254.242.144",
+        open:true,
+        livereload:true,
+        middleware:function(req,res,next){ 
+            if(req.url==='/index'){
+                res.end(JSON.stringify(data));
+            }
+            next();
+        }
+    }))
+})
