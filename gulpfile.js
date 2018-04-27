7c9675e494bbd0f9c0deb5df444fd9b538fcadea
guomaomi <982548809@qq.com>
2018-04-27 11:37:52 +0800

    添加index.html

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var server=require("gulp-webserver");
 var data=require("./src/data/data.json"); 
 
 gulp.task("default",function(){
-    gulp.src("src")
+    gulp.src("./src")
     .pipe(server({
         port:"8070",
         host:"169.254.242.144",
